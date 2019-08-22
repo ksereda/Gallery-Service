58f365b79ff908c9c34280af660f917f0821dbc4
ksereda <kirill.serada@gmail.com>
2019-08-22 19:31:11 +0300

    added security service to project

---
+++ security-service/src/main/java/com/example/securityservice/SecurityServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.securityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServiceApplication.class, args);
+	}
+
+}
---
+++ security-service/src/main/java/com/example/securityservice/security/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.securityservice.security;
+
+public enum UserRole {
+	USER, ADMIN;
+}
