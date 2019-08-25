753430f218cd5d5e669db7a3654bd3c1dc0702bd
ksereda <kirill.serada@gmail.com>
2019-08-25 20:10:07 +0300

    added hystrix dashboard to project

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +19,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableHystrix
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
