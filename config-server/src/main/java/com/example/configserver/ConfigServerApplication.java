f1a786c43f45d96217862e2123cab15177adee50
ksereda <kirill.serada@gmail.com>
2019-08-27 13:38:31 +0300

    added config server

---
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
