5bf7c4e402fce0d1f96ffc2df03e02498f8ad622
ksereda <kirill.serada@gmail.com>
2019-08-28 23:40:32 +0300

    deleted old repository from gallery-service

--- gallery-service/src/main/java/com/example/galleryservice/repository/SimpleRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.example.galleryservice.repository;
-//
-//import com.example.galleryservice.model.Bucket;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface SimpleRepository extends CrudRepository<Bucket, Long> {
-//}
