1ebc0ea3a78167d14becb3b0ea22bb2b2bae3998
ksereda <kirill.serada@gmail.com>
2019-08-29 19:31:09 +0300

    added WebClient logic to user-service

--- config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/configserver/ConfigServerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @EnableConfigServer
+//@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
--- user-service/src/main/java/com/example/userservice/service/WebClientService.java
+++ user-service/src/main/java/com/example/userservice/service/WebClientService.java
@@ -1,15 +1,18 @@
 package com.example.userservice.service;
 
+import com.example.userservice.error.MyCustomClientException;
+import com.example.userservice.error.MyCustomServerException;
 import com.example.userservice.model.Bucket;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class WebClientService {
@@ -26,14 +29,81 @@ public class WebClientService {
                 .baseUrl(API_BASE_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, API_MIME_TYPE)
                 .defaultHeader(HttpHeaders.USER_AGENT, USER_AGENT)
+//                .filter(WebClientService.errorHandlingFilter())  // хз
                 .build();
     }
 
     public Flux<Bucket> getDataByWebClient() {
-        return webClient.get()
+        return webClient
+                .get()
                 .uri("/stream/buckets/delay")
                 .exchange()
                 .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Bucket.class));
     }
+
+
+
+//    public Flux<Bucket> getDataByWebClient() {
+//        return webClient
+//                .get()
+//                .uri("/getAll")
+//                .retrieve()
+////                .onStatus(HttpStatus::is4xxClientError, clientResponse ->
+////                        Mono.error(new RuntimeException("4xx"))
+////                )
+////                .onStatus(HttpStatus::is5xxServerError, clientResponse ->
+////                        Mono.error(new RuntimeException("5xx"))
+////                )
+////                .onStatus(HttpStatus::is3xxRedirection, clientResponse ->
+////                        Mono.error(new MyCustomServerException())
+////                )
+////                .onStatus(HttpStatus::isError, clientResponse ->
+////                        Mono.error(new MyCustomServerException())
+////                )
+//
+//                .onStatus(HttpStatus::is4xxClientError, response -> {
+//                    System.out.println("4xx eror");
+//                    return Mono.error(new RuntimeException("4xx"));
+//                })
+//                .onStatus(HttpStatus::is5xxServerError, response -> {
+//                    System.out.println("5xx eror");
+//                    return Mono.error(new RuntimeException("5xx"));
+//                })
+//
+//                .onStatus(HttpStatus::isError, clientResponse -> {
+//                    System.out.println("eror");
+//                    return Mono.error(new MyCustomServerException());
+//                })
+//
+//                .bodyToFlux(Bucket.class);
+//    }
+
+
+
+
+
+
+//     public Flux<Bucket> getDataByWebClient() {
+//        return webClient
+//                .get()
+//                .uri("/getAll")
+//                .exchange()
+//                .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Bucket.class));
+//    }
+//
+//    public static ExchangeFilterFunction errorHandlingFilter() {
+//        return ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
+//            if(clientResponse.statusCode()!=null && (clientResponse.statusCode().is5xxServerError() || clientResponse.statusCode().is4xxClientError()) ) {
+//                return clientResponse.bodyToMono(String.class)
+//                        .flatMap(errorBody -> {
+////                            return Mono.error(new CustomWebClientResponseException(errorBody,clientResponse.statusCode()));
+//                            return Mono.error(new MyCustomServerException());
+//                        });
+//            }else {
+//                return Mono.just(clientResponse);
+//            }
+//        });
+//    }
+
 
 }
